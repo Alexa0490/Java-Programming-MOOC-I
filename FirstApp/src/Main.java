fbb576f4d9e7ddac67622a8dbea049f21f26208f
Alexa0490 <134538907+Alexa0490@users.noreply.github.com>
2023-06-01 20:40:32 +0300

    Prima tema

---
+++ FirstApp/src/Main.java
@@ -0,0 +1,8 @@
+java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello \nAlexandra");
+    }
+}
