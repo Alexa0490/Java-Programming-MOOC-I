3ca8e63a2b95ff3e0560cdcb4e4d193890741dea
Alexa0490 <134538907+Alexa0490@users.noreply.github.com>
2023-06-19 00:48:46 +0300

    Add files via upload

---
+++ Ex.1/src/Main.java
@@ -0,0 +1,12 @@
+/* Write a class that represents a person. Extend that class for particular types of people:
+professor, student, janitor, security guard.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+
+    }
+}
